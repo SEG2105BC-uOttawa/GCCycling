216b38285fb4e70a1de0f7924f676fc3ee2ea61f
Ke-Le-04 <145041566+Ke-Le-04@users.noreply.github.com>
2023-10-08 14:52:34 -0400

    Update Welcome.java

--- app/src/main/java/com/example/gcccyclingapp/Welcome.java
+++ app/src/main/java/com/example/gcccyclingapp/Welcome.java
@@ -7,10 +7,17 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Welcome extends AppCompatActivity {
+
+
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.welcome_screen);
+            MainActivity MA = new MainActivity();
+            TextView tv1 = (TextView) findViewById(R.id.txtWelcome);
+            String name = MA.getUsername().toString();
+            String role = "admin";
+            tv1.setText("Welcome " + name + "you are logged in as " + role);
         }
 
 
