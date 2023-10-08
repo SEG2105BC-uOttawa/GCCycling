9d1ffd248ca8f47e9d7b3023c75de538d632e83c
Jason <jder053@uottawa.ca>
2023-10-07 20:09:22 -0400

    updated account.java and club.java

--- Account.java
+++ Account.java
@@ -2,11 +2,40 @@
 
 public class Account {
 
-    String username, pwd;
+    String username, password, role;
 
-    public Account(String username, String pwd) {
+    public Account(String username, String password, String role) {
         this.username = username;
-        this.pwd = pwd;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean validLogin(String inputUsername, String inputPassword) {
+        // is case sensitive
+        if (!this.username.equals(inputUsername) || !this.password.equals(inputPassword)) {
+            return false;
+        }
+        return true;
     }
 
 }
