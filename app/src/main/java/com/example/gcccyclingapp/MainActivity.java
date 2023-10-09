d492b19e468779ebdff253bfc36b09d09065fa61
tazim04 <35643104+tazim04@users.noreply.github.com>
2023-10-09 12:30:12 -0400

    Removed DB testing code

--- app/src/main/java/com/example/gcccyclingapp/MainActivity.java
+++ app/src/main/java/com/example/gcccyclingapp/MainActivity.java
@@ -11,16 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Admin admin = new Admin();
-
-        DBAdmin dbAdmin = new DBAdmin(MainActivity.this);
-        DBClubs dbClubs = new DBClubs(MainActivity.this);
-//        admin.deleteClub(dbAdmin, dbClubs, "club2"); fix delete
-//        admin.createClub(dbAdmin, dbClubs, "club2");
-//        dbClubs.addParticipant("club", "bob");
-
-
-
     }
 }
