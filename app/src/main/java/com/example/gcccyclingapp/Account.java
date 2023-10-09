23015679416daaaedddd1f03b42ab0776221f148
Jason <jder053@uottawa.ca>
2023-10-09 00:57:39 -0400

    added account and club account class

---
+++ app/src/main/java/com/example/gcccyclingapp/Account.java
@@ -0,0 +1,43 @@
+package com.example.gcccyclingapp;
+
+//package com.example.myapplication;
+
+public class Account {
+
+    String username, password, role;
+
+    public Account(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean validLogin(String inputUsername, String inputPassword) {
+        // is case sensitive
+        if (!this.username.equals(inputUsername) || !this.password.equals(inputPassword)) {
+            return false;
+        }
+        return true;
+    }
+
+}
