3c20aa6c4516febaebe1cef119d9ba3abe515b8f
tazim04 <35643104+tazim04@users.noreply.github.com>
2023-10-09 20:49:13 -0400

    Add functions working, delete functions put on hold

--- app/src/main/java/com/example/gcccyclingapp/DBAdmin.java
+++ app/src/main/java/com/example/gcccyclingapp/DBAdmin.java
@@ -1,6 +1,7 @@
 package com.example.gcccyclingapp;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -12,7 +13,9 @@ public class DBAdmin extends SQLiteOpenHelper{
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "admin.db";
     public static final String ADMIN_TABLE = "ADMIN_TABLE";
-    public static final String ADMIN_CLUBS_COLUMN = "ADMIN_CLUBS"; // column listing clubs admin has
+    public static final String ADMIN_CLUB_NAME = "ADMIN_CLUB_NAME"; // column listing clubs admin has
+    public static final String ADMIN_CLUB_USERNAME = "ADMIN_CLUB_USERNAME"; // column listing clubs admin has
+    public static final String ADMIN_CLUB_PASSWORD = "ADMIN_CLUB_PASSWORD"; // column listing clubs admin has
 
 
 
@@ -24,7 +27,7 @@ public class DBAdmin extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) { // this is called when the database is first accessed
-        String createTableStatement = "CREATE TABLE " +ADMIN_TABLE + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " + ADMIN_CLUBS_COLUMN +" TEXT)"; // create admin table that has list of clubs
+        String createTableStatement = "CREATE TABLE " +ADMIN_TABLE + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " + ADMIN_CLUB_NAME +" TEXT, " + ADMIN_CLUB_USERNAME +" TEXT, " + ADMIN_CLUB_PASSWORD + " TEXT)"; // create admin table that has list of clubs
         db.execSQL(createTableStatement);
     }
 
@@ -33,23 +36,25 @@ public class DBAdmin extends SQLiteOpenHelper{
 
     }
 
-    public void insert(String clubName){ // change to work with Club class
+    public void insert(String clubName, String clubUser, String clubPWD){ // change to work with Club class
         SQLiteDatabase db = this.getWritableDatabase(); // for insert actions
         ContentValues cv = new ContentValues();
 
-        cv.put(ADMIN_CLUBS_COLUMN, clubName); // inserts data into club column
-        db.insert(ADMIN_TABLE, null, cv);
+        cv.put(ADMIN_CLUB_NAME, clubName); // inserts data into club column
+        cv.put(ADMIN_CLUB_USERNAME, clubUser); // inserts data into club column
+        cv.put(ADMIN_CLUB_PASSWORD, clubPWD); // inserts data into club column
 
-        Log.d("Data Insert", "Data inserted");
+        db.insert(ADMIN_TABLE, null, cv);
     }
 
     public void delete(String clubName){ // change to work with Club class
         SQLiteDatabase db = this.getWritableDatabase(); // for insert actions
 
-        db.delete(ADMIN_TABLE, null, null);
-        db.execSQL("DELETE FROM " + ADMIN_TABLE + " WHERE " + ADMIN_CLUBS_COLUMN + " = " + clubName);
+        String queryString = "DELETE FROM " + ADMIN_TABLE + " WHERE " + ADMIN_CLUB_NAME + " = " + clubName;
+        Cursor cursor = db.rawQuery(queryString, null);
 
-        Log.d("Data Deleted", "Data Deleted");
+        db.delete(ADMIN_TABLE, ADMIN_CLUB_NAME+" = "+clubName, null);
+        db.execSQL("DELETE FROM " + ADMIN_TABLE + " WHERE " + ADMIN_CLUB_NAME + " = " + clubName);
     }
 
 }
--- app/src/main/java/com/example/gcccyclingapp/DBClubs.java
+++ app/src/main/java/com/example/gcccyclingapp/DBClubs.java
@@ -11,8 +11,9 @@ public class DBClubs extends SQLiteOpenHelper{
 
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "clubs.db";
-    public static final String CLUB_PARTICIPANTS_COLUMN = "CLUB_PARTICIPANTS"; // column listing participants in club
-
+    public static final String CLUB_PARTICIPANT_NAME = "PARTICIPANT_NAME"; // column listing participants in club
+    public static final String CLUB_PARTICIPANT_USERNAME = "PARTICIPANT_USERNAME";
+    public static final String CLUB_PARTICIPANT_PASSWORD = "PARTICIPANT_PASSWORD";
 
 
     public DBClubs(@Nullable Context context) {
@@ -33,24 +34,48 @@ public class DBClubs extends SQLiteOpenHelper{
 
     public void createTable(String clubName){
         SQLiteDatabase db = getWritableDatabase();
-        String createTableStatement = "CREATE TABLE " + clubName + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " + CLUB_PARTICIPANTS_COLUMN +" TEXT)";
+        String createTableStatement = "CREATE TABLE " + clubName +
+                " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                CLUB_PARTICIPANT_NAME +" TEXT, " +
+                CLUB_PARTICIPANT_USERNAME +" TEXT, " +
+                CLUB_PARTICIPANT_PASSWORD +" TEXT)";
+
         db.execSQL(createTableStatement);
+        db.close();
 
-        Log.d("New Club", "CLub created");
+        Log.d("Club", clubName+" created");
     }
+//    public void deleteTable(String clubName){
+//        SQLiteDatabase db = getWritableDatabase();
+//        String deleteTableStatement = "DROP TABLE " + clubName;
+//        db.execSQL(deleteTableStatement);
+//        db.close();
+//
+//        Log.d("Club", clubName+" deleted");
+//    }
 
-    public void addParticipant(String clubName, String participantName){
-        SQLiteDatabase db = this.getWritableDatabase(); // for insert actions
+    public void addParticipant(String clubName, String participantName, String participantUser, String participantPWD){
+        SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
 
-        cv.put(CLUB_PARTICIPANTS_COLUMN, participantName); // inserts data into club column
+        cv.put(CLUB_PARTICIPANT_NAME, participantName); // inserts data into club column
+        cv.put(CLUB_PARTICIPANT_USERNAME, participantUser);
+        cv.put(CLUB_PARTICIPANT_PASSWORD, participantPWD);
         db.insert(clubName, null, cv);
 
         Log.d("Data Insert", "Participant added to "+clubName);
     }
+//    public void deleteParticipant(String clubName, String participantName){
+//        SQLiteDatabase db = this.getWritableDatabase();
+//
+//        db.delete(clubName, null, null);
+//        db.execSQL("DELETE FROM " + clubName + " WHERE " + CLUB_PARTICIPANT_NAME + " = " + participantName);
+//        db.close();
+//
+//        Log.d("Data Deleted", participantName+" deleted");
+//    }
 
-    // delete table
-    // update club participants list (add / delete to tables)
+
 
 
 
