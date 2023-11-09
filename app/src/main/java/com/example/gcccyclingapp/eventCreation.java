f1392180266338b9a415844ca28822eaca4bbff5
Ke-Le-04 <145041566+Ke-Le-04@users.noreply.github.com>
2023-11-09 15:00:03 -0500

    Update eventCreation.java

--- app/src/main/java/com/example/gcccyclingapp/eventCreation.java
+++ app/src/main/java/com/example/gcccyclingapp/eventCreation.java
@@ -8,9 +8,11 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class eventCreation extends AppCompatActivity {
+public class eventCreation extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     String eventSelected;
 
@@ -32,26 +34,57 @@ public class eventCreation extends AppCompatActivity {
             eventSelected = parent.getItemAtPosition(pos).toString();
         }
 
-        public void onNothingSelected(AdapterView<?> parent) {}
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {}
 
-        public void set() {
-            TextView age = findViewById(R.id.agetxt);
-            TextView pace = findViewById(R.id.pacetxt);
-            TextView level = findViewById(R.id.leveltxt);
-            TextView location = findViewById(R.id.locationtxt);
-            TextView time = findViewById(R.id.timetxt);
-            TextView details = findViewById(R.id.detailstxt);
+
+        public void set(View view) {
+            EditText age = (EditText) findViewById(R.id.agetxt);
+            EditText pace = (EditText) findViewById(R.id.pacetxt);
+            EditText level = (EditText) findViewById(R.id.leveltxt);
+            EditText location = (EditText) findViewById(R.id.locationtxt);
+            EditText time = (EditText) findViewById(R.id.timetxt);
+            EditText details = (EditText) findViewById(R.id.detailstxt);
+            Spinner event = (Spinner) findViewById(R.id.eventType);
 
             String strAge = age.getText().toString();
             String strPace = pace.getText().toString();
-            String strLevel = level.getText().toString();
-            String strLocation = location.getText().toString();
-            String strTime = time.getText().toString();
-            String strDetails = details.getText().toString();
+            String  strLevel = level.getText().toString();
+            String  strLocation = location.getText().toString();
+            String  strTime = time.getText().toString();
+            String  strDetails = details.getText().toString();
+            event.setOnItemSelectedListener(this);
+
+             Boolean check = false;
+
+            if(strLocation.equals("") || strTime.equals("") || strDetails.equals("")){
+                TextView warning = (TextView) findViewById(R.id.warningtxt);
+                warning.setText("Either Location, Time, or Details are missing please fill those out");
+            } else{
+                check = true;
+            }
+
+            if(strAge.equals("")){
+                strAge = "25";
+            }
+            if(strPace.equals("")){
+                strPace = "20";
+            }
+            if(strLevel.equals("")){
+                strLevel = "5";
+            }
+
+            if(check == true) {
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("age", strAge);
+                returnIntent.putExtra("pace", strPace);
+                returnIntent.putExtra("level", strLevel);
+                returnIntent.putExtra("location", strLocation);
+                returnIntent.putExtra("time", strTime);
+                returnIntent.putExtra("details", strDetails);
+                returnIntent.putExtra("event_type", eventSelected);
+                setResult(RESULT_OK, returnIntent);
+                finish();
+            }
+
         }
-
-
-
-
-
-}
