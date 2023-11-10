92c3037d347dab1ade74e562760dd7edf120c84a
tazim04 <35643104+tazim04@users.noreply.github.com>
2023-11-10 16:27:29 -0500

    created eventType class, added eventType functions for admin and db

--- app/src/main/java/com/example/gcccyclingapp/EventCreation.java
+++ app/src/main/java/com/example/gcccyclingapp/EventCreation.java
@@ -22,13 +22,13 @@ public class EventCreation extends AppCompatActivity implements AdapterView.OnIt
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_event);
 
-        Spinner eventType = findViewById(R.id.eventType);
-        String[] event_types = {"TT", "Hill Climb", "Road Stage Race", "Road Race", "Group Rides"};
-        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_spinner_item, event_types);
+//        Spinner eventType = findViewById(R.id.eventType);
+//        String[] event_types = {"TT", "Hill Climb", "Road Stage Race", "Road Race", "Group Rides"};
+//        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_spinner_item, event_types);
 
-        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        eventType.setAdapter(ad);
+//        eventType.setAdapter(ad);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
@@ -54,21 +54,23 @@ public class EventCreation extends AppCompatActivity implements AdapterView.OnIt
 
 
     public void set(View view) {
+        EditText type = (EditText) findViewById(R.id.eventTypetxt);
         EditText age = (EditText) findViewById(R.id.agetxt);
         EditText pace = (EditText) findViewById(R.id.pacetxt);
         EditText level = (EditText) findViewById(R.id.leveltxt);
         EditText location = (EditText) findViewById(R.id.locationtxt);
         EditText time = (EditText) findViewById(R.id.timetxt);
         EditText details = (EditText) findViewById(R.id.detailstxt);
-        Spinner event = (Spinner) findViewById(R.id.eventType);
+//        Spinner event = (Spinner) findViewById(R.id.eventType);
 
+        String strType = type.getText().toString();
         String strAge = age.getText().toString();
         String strPace = pace.getText().toString();
         String strLevel = level.getText().toString();
         String strLocation = location.getText().toString();
         String strTime = time.getText().toString();
         String strDetails = details.getText().toString();
-        event.setOnItemSelectedListener(this);
+//        event.setOnItemSelectedListener(this);
 
         Boolean check = false;
 
@@ -91,13 +93,14 @@ public class EventCreation extends AppCompatActivity implements AdapterView.OnIt
 
         if (check == true) {
             Intent returnIntent = new Intent();
+            returnIntent.putExtra("type", strAge);
             returnIntent.putExtra("age", strAge);
             returnIntent.putExtra("pace", strPace);
             returnIntent.putExtra("level", strLevel);
             returnIntent.putExtra("location", strLocation);
             returnIntent.putExtra("time", strTime);
             returnIntent.putExtra("details", strDetails);
-            returnIntent.putExtra("event_type", eventSelected);
+//            returnIntent.putExtra("event_type", eventSelected);
             setResult(RESULT_OK, returnIntent);
             finish();
         }
