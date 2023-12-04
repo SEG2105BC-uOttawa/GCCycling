6fbac667bbe6d6e40327aa9adb41ee541e0d2b4e
Ke-Le-04 <145041566+Ke-Le-04@users.noreply.github.com>
2023-12-04 14:12:09 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/gcccyclingapp/ParticipantBrowse.java
@@ -0,0 +1,14 @@
+package com.example.gcccyclingapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ParticipantBrowse extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.browse_screen);
+    }
+
+}
